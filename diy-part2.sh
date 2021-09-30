a060c7bc1f2aedd3cc11ac937e9026e0f0c1d6f2
3652365 <desmond.beyond@gmail.com>
2021-09-30 04:23:05 +0000

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,12 +13,13 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.100.182/g' package/base-files/files/bin/config_generate
 mkdir package/tt/
-git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
-git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/openwrt-smartdns
+# git clone https://github.com/pymumu/luci-app-smartdns.git feeds/luci/applications/luci-app-smartdns
+# git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/openwrt-smartdns
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/tt/openwrt-passwall
 git clone https://github.com/sirpdboy/luci-app-netdata.git package/tt/luci-app-netdata
 git clone https://github.com/netdata/netdata.git package/tt/netdata
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/tt/luci-app-adguardhome
-git clone https://github.com/AdguardTeam/AdGuardHome.git package/tt/AdGuardHome
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/tt/luci-app-adguardhome
+# git clone https://github.com/AdguardTeam/AdGuardHome.git package/tt/AdGuardHome
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/tt/luci-app-unblockneteasemusic
 
 
